bf51601522c49bccff250efa87201f8908ea9c11
thisisnabi <thisisnabi@outlook.com>
2024-04-10 14:48:03 +0330

    Add EntitiyBase class into the domain as an abstraction class

---
+++ src/Blogger.Domain/Common/EntityBase.cs
@@ -0,0 +1,35 @@
+﻿namespace Blogger.Domain.Common;
+
+public abstract class EntityBase<Tkey>
+{
+    public Tkey Id { get; private set; } = default!;
+
+    protected EntityBase(Tkey id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        var entity = obj as EntityBase<Tkey>;
+
+        if (entity is null) return false;
+
+        return  IsTypeEquals(entity, this) &&
+                EqualityComparer<Tkey>.Default.Equals(Id, entity.Id);
+    }
+
+    private static bool IsTypeEquals(EntityBase<Tkey> left, EntityBase<Tkey> right) 
+        => left.GetType() == right.GetType();
+
+    public static bool operator ==(EntityBase<Tkey> left, EntityBase<Tkey> right)
+    {
+        return  IsTypeEquals(left, right) &&
+                EqualityComparer<Tkey>.Default.Equals(left.Id, right.Id);
+    }
+
+    public static bool operator !=(EntityBase<Tkey> left, EntityBase<Tkey> right) 
+        => !(left == right);
+
+    public override int GetHashCode() => HashCode.Combine(GetType(), Id);
+}
---
+++ tests/Blogger.UnitTests/Domain/EntityBaseTests.cs
@@ -0,0 +1,50 @@
+﻿using Blogger.Domain.Common;
+using FluentAssertions;
+
+namespace Blogger.UnitTests.Domain;
+public class EntityBaseTests
+{
+    [Fact]
+    public void entities_of_different_type_should_not_be_equal()
+    {
+        var id = Guid.NewGuid();
+        var testA = new TestClassA(id);
+        var testB = new TestClassB(id);
+
+        (testA == testB).Should().BeFalse();
+        testB.Equals(testA).Should().BeFalse();
+        testA.Equals(testB).Should().BeFalse();
+
+        (testA.GetHashCode() == testB.GetHashCode()).Should().BeFalse();
+    }
+
+    [Fact]
+    public void entities_of_same_type_should_be_equal_when_ids_match()
+    {
+        var id = Guid.NewGuid();
+        var entityA = new TestClassA(id);
+        var entityB = new TestClassA(id);
+
+        (entityA == entityB).Should().BeTrue();
+        entityA.Equals(entityB).Should().BeTrue();
+        entityB.Equals(entityA).Should().BeTrue();
+
+        (entityA.GetHashCode() == entityB.GetHashCode()).Should().BeTrue();
+    }
+
+    [Fact]
+    public void entities_of_same_type_should_not_be_equal_when_ids_different()
+    {
+        var entityA = new TestClassA(Guid.NewGuid());
+        var entityB = new TestClassA(Guid.NewGuid());
+
+        (entityA == entityB).Should().BeFalse();
+        entityA.Equals(entityB).Should().BeFalse();
+        entityB.Equals(entityA).Should().BeFalse();
+
+        (entityA.GetHashCode() == entityB.GetHashCode()).Should().BeFalse();
+    }
+
+    private class TestClassA(Guid id) : EntityBase<Guid>(id) { }
+    private class TestClassB(Guid id) : EntityBase<Guid>(id) { }
+}
