8216aeceaa52c5774cf5f947abebb40f261b94c3
thisisnabi <thisisnabi@outlook.com>
2024-04-11 17:00:21 +0330

    Add tag VO into the domain

---
+++ src/Blogger.Domain/ArticleAggregate/Tag.cs
@@ -0,0 +1,17 @@
+﻿namespace Blogger.Domain.ArticleAggregate;
+public class Tag : ValueObject<Tag>
+{
+    public string Value { get; init; }
+
+    private Tag(string value)
+    {
+        Value = value;
+    }
+
+    public override IEnumerable<object> GetEqualityComponenets()
+    {
+        yield return Value;
+    }
+
+    public static Tag Create(string tagValue) => new(tagValue);
+}
