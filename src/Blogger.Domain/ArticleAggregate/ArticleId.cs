63ae2865328bdec31e3149bcc37298537661a0ef
thisisnabi <thisisnabi@outlook.com>
2024-04-11 16:59:07 +0330

    Add ArticleId into the domain model

---
+++ src/Blogger.Domain/ArticleAggregate/ArticleId.cs
@@ -0,0 +1,23 @@
+﻿namespace Blogger.Domain.ArticleAggregate;
+
+public class ArticleId : ValueObject<ArticleId>
+{
+    public string Value { get; set; }
+
+    public override IEnumerable<object> GetEqualityComponenets()
+    {
+        yield return Value;
+    }
+
+    private ArticleId(string slug)
+    {
+        Value = slug;
+    }
+
+    public static ArticleId CreateUniqueId(string title)
+    {
+        // TODO generate kebab case from the title
+        var slug = title;
+        return new(slug);
+    }
+}
