d22af029d50aaf3d45ab2ab4fec3ac4cdd028f40
thisisnabi <thisisnabi@outlook.com>
2024-04-11 16:58:52 +0330

    Add CommentId into the domain

---
+++ src/Blogger.Domain/ArticleAggregate/CommentId.cs
@@ -0,0 +1,18 @@
+﻿namespace Blogger.Domain.ArticleAggregate;
+
+public class CommentId : ValueObject<CommentId>
+{
+    public Guid Value { get; set; }
+  
+    public override IEnumerable<object> GetEqualityComponenets()
+    {
+        yield return Value;
+    }
+
+    private CommentId(Guid id)
+    {
+        Value = id;
+    }
+
+    public static CommentId CreateUniqueId() => new CommentId(Guid.NewGuid());
+}
