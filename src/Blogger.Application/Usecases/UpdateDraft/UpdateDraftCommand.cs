1e0fefad662af23563ad45c8e883a7862f97ac5f
Nabi Karampour <N.Karampour@Alibaba.ir>
2024-04-12 04:13:42 +0330

    Change update article tag condition

--- src/Blogger.Application/Usecases/UpdateDraft/UpdateDraftCommand.cs
+++ src/Blogger.Application/Usecases/UpdateDraft/UpdateDraftCommand.cs
@@ -1,4 +1,4 @@
 ﻿namespace Blogger.Application.Usecases.UpdateDraft;
 
-public record UpdateDraftCommand(ArticleId ArticleId, string title, string body, string summery, string[] Tags)
+public record UpdateDraftCommand(ArticleId ArticleId, string title, string body, string summery, IReadOnlyList<Tag> Tags)
     : IRequest;
