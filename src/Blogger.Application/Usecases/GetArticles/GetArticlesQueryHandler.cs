9faf5b4626993bdf444e4077eb4c243601a27b01
thisisnabi <thisisnabi@outlook.com>
2024-04-13 01:40:55 +0330

    Rename latest article methods in application

--- src/Blogger.Application/Usecases/GetArticles/GetArticlesQueryHandler.cs
+++ src/Blogger.Application/Usecases/GetArticles/GetArticlesQueryHandler.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections.Immutable;
-using System.Linq;
 
 namespace Blogger.Application.Usecases.GetArticles;
 
-public class GetArticlesQueryHandler(IArticleRepository articleRepository) 
+public class GetArticlesQueryHandler(IArticleRepository articleRepository)
     : IRequestHandler<GetArticlesQuery, IReadOnlyList<GetArticlesQueryResponse>>
 {
     private readonly IArticleRepository _articleRepository = articleRepository;
 
     public async Task<IReadOnlyList<GetArticlesQueryResponse>> Handle(GetArticlesQuery request, CancellationToken cancellationToken)
     {
-        var articles = await _articleRepository.GetArticles(request.PageNumber, request.PageSize, cancellationToken);
+        var articles = await _articleRepository.GetLatestArticles(request.PageNumber, request.PageSize, cancellationToken);
 
+        // TODO: using mapster for mapping 
         return articles.Select(x => new GetArticlesQueryResponse(
             x.Id,
             x.Title,
--- src/Blogger.Domain/ArticleAggregate/IArticleRepository.cs
+++ src/Blogger.Domain/ArticleAggregate/IArticleRepository.cs
@@ -5,7 +5,7 @@ public interface IArticleRepository
     Task CreateAsync(Article article, CancellationToken cancellationToken);
     Task<Article> GetArticleByCommentId(CommentId commentId, CancellationToken cancellationToken);
     Task<Article> GetArticleById(ArticleId articleId, CancellationToken cancellationToken);
-    Task<IReadOnlyList<Article>> GetArticles(int pageNumber, int pageSize, CancellationToken cancellationToken);
     Task<Article> GetDraftById(ArticleId articleId, CancellationToken cancellationToken);
+    Task<IReadOnlyList<Article>> GetLatestArticles(int pageNumber, int pageSize, CancellationToken cancellationToken);
     Task SaveChangesAsync(CancellationToken cancellationToken);
 }
