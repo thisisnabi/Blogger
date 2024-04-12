848a1b784dc902e4a9b469b61172b1fccaee50db
Nabi Karampour <N.Karampour@Alibaba.ir>
2024-04-12 04:42:29 +0330

    Change set modifier in Client property

--- src/Blogger.Domain/ArticleAggregate/Comment.cs
+++ src/Blogger.Domain/ArticleAggregate/Comment.cs
@@ -4,11 +4,11 @@ public class Comment(CommentId id) : EntityBase<CommentId>(id)
 {
     public Client ClientId { get; init; }
 
-    public DateTime CreatedOnUtc { get; set; }
+    public DateTime CreatedOnUtc { get; init; }
 
-    public string Content { get; set; }
+    public string Content { get; init; }
 
-    public bool IsApproved { get; set; }
+    public bool IsApproved { get; private set; }
 
     public static Comment Create(Client client, string content) =>
         new Comment(CommentId.CreateUniqueId())
