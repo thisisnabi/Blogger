e8238c02174ccda775788b3a682689e3b0b73679
Nabi Karampour <N.Karampour@Alibaba.ir>
2024-04-12 04:16:06 +0330

    Add cancellation token in GetDraftById

--- src/Blogger.Domain/ArticleAggregate/IArticleRepository.cs
+++ src/Blogger.Domain/ArticleAggregate/IArticleRepository.cs
@@ -3,8 +3,8 @@ namespace Blogger.Domain.ArticleAggregate;
 public interface IArticleRepository
 {
     Task CreateAsync(Article article, CancellationToken cancellationToken);
-    Task<Article> GetArticleByCommentId(CommentId commentId);
-    Task<Article> GetArticleById(ArticleId articleId);
-    Task<Article> GetDraftById(ArticleId articleId);
+    Task<Article> GetArticleByCommentId(CommentId commentId, CancellationToken cancellationToken);
+    Task<Article> GetArticleById(ArticleId articleId, CancellationToken cancellationToken);
+    Task<Article> GetDraftById(ArticleId articleId, CancellationToken cancellationToken);
     Task SaveChangesAsync(CancellationToken cancellationToken);
 }
