33667392a8626f881648e0a95bda1f6b3f7d487e
Ali Ghodrati <ali.ghodrati@live.com>
2024-04-14 00:15:55 +0330

    feat: custom exception for invalid action on article status

---
+++ tests/Blogger.UnitTests/Article/ArticleTests.cs
@@ -0,0 +1,52 @@
+﻿using Blogger.Domain.ArticleAggregate;
+
+namespace Blogger.UnitTests.Article;
+
+public class ArticleTests
+{
+    [Fact]
+    public void AddCommentOnDraftArticle_Should_ThrowException()
+    {
+        //Arrange
+        var article = Blogger.Domain.ArticleAggregate.Article.CreateDraft("title", "body", "summary");
+        var comment = Comment.Create(Client.Create("FullName", "a@a.com"), "content");
+
+        //Act
+        //Assert
+        article.Invoking(x => x.AddComment(comment))
+            .Should()
+            .Throw<InvalidArticleActionException>()
+            .WithMessage($"Invalid action on {ArticleStatus.Draft} status");
+
+    }
+    [Fact]
+    public void AddCommentOnDeleteArticle_Should_ThrowException()
+    {
+        //Arrange
+        var article = Blogger.Domain.ArticleAggregate.Article.CreateDraft("title", "body", "summary");
+        article.Remove();
+        var comment = Comment.Create(Client.Create("FullName", "a@a.com"), "content");
+
+        //Act
+        //Assert
+        article.Invoking(x => x.AddComment(comment))
+            .Should()
+            .Throw<InvalidArticleActionException>()
+            .WithMessage($"Invalid action on {ArticleStatus.Deleted} status");
+
+    }
+    [Fact]
+    public void AddCommentOnPublishedArticle_Should_Be_Success()
+    {
+        //Arrange
+        var article = Blogger.Domain.ArticleAggregate.Article.CreateArticle("title", "body", "summary");
+        var comment = Comment.Create(Client.Create("FullName", "a@a.com"), "content");
+
+        //Act
+        article.AddComment(comment);
+
+        //Assert
+        article.Commnets.Should().HaveCount(1);
+
+    }
+}
