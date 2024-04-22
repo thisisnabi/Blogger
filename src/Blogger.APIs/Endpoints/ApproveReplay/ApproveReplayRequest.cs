be020b42222ec869f7691eeefeb25681f1a27952
thisisnabi <thisisnabi@outlook.com>
2024-04-22 12:45:07 +0330

    Update approve replay

--- src/Blogger.APIs/Endpoints/ApproveReplay/ApproveReplayRequest.cs
+++ src/Blogger.APIs/Endpoints/ApproveReplay/ApproveReplayRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace Blogger.APIs.Contracts.ApproveReplay;
 
-public record ApproveReplayRequest([FromQuery]string Link, [FromRoute(Name = "comment-id")] Guid CommentId);
+public record ApproveReplayRequest([FromQuery]string Link, [FromQuery(Name ="comment-id")] Guid CommentId);
