e5d6a355d3a5497936c9119a50431f3fbee6d9d8
Nabi Karampour <N.Karampour@Alibaba.ir>
2024-05-06 23:03:03 +0330

    Add base aggregate unit tests

---
+++ tests/Blogger.UnitTests/BuldingBlocks/Domain/AggregateRootBaseTests.cs
@@ -0,0 +1,60 @@
+﻿using Blogger.Domain.Common;
+using FluentAssertions;
+
+namespace Blogger.UnitTests.BuldingBlocks.Domain;
+public class AggregateRootBaseTests
+{
+    [Fact]
+    public void events_should_be_have_item_when_adding_event()
+    {
+        // arrane
+        var aggregateRoot = new ConcreteAggregateRoot(10);
+
+        // act
+        aggregateRoot.RaiseCreatedEvent();
+
+        //
+        aggregateRoot.Events.Should().NotBeEmpty();
+        aggregateRoot.Events.Should().HaveCount(1);
+    }
+
+   [Fact]
+    public void events_should_be_empty_when_create_new_aggregate()
+    {
+        // arrane
+        var aggregateRoot = new ConcreteAggregateRoot(10);
+  
+        // act
+        aggregateRoot.Events.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void events_should_be_empty_when_call_CleanEvents()
+    {
+        // arrane
+        var aggregateRoot = new ConcreteAggregateRoot(10);
+        
+        // act
+        aggregateRoot.RaiseCreatedEvent();
+        aggregateRoot.ClearEvents();
+
+        // act
+        aggregateRoot.Events.Should().BeEmpty();
+    }
+
+    private class ConcreteAggregateRoot(int id) : AggregateRootBase<int>(id)
+    {
+        public void RaiseCreatedEvent() => AddEvent(new CreatedRootEvent { });
+    }
+
+    private class OtherConcreteAggregateRoot(int id) : AggregateRootBase<int>(id)
+    {
+
+    }
+
+    private class CreatedRootEvent : IDomainEvent
+    {
+        public DateTime OccurredOn => DateTime.UtcNow;
+    }
+
+}
