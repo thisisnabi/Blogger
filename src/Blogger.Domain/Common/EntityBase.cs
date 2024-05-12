0ef341978ea89beb51c11d4631d83d9517f71765
Amirhessam Pourhossein <amirhesamph81@gmail.com>
2024-05-12 14:15:36 +0330

    cleanup EntityBase

--- src/Blogger.Domain/Common/EntityBase.cs
+++ src/Blogger.Domain/Common/EntityBase.cs
@@ -9,18 +9,18 @@ public abstract class EntityBase<TId> where TId : notnull
         Id = id;
     }
 
-    public override bool Equals(object? obj) 
+    public override bool Equals(object? obj)
         => obj is not null &&
            obj is EntityBase<TId> entity &&
            obj.GetType() == GetType() &&
            Id.Equals(entity.Id);
 
-    public static bool operator ==(EntityBase<TId> left, EntityBase<TId> right) 
+    public static bool operator ==(EntityBase<TId> left, EntityBase<TId> right)
         => left.Equals(right);
 
     public static bool operator !=(EntityBase<TId> left, EntityBase<TId> right)
         => !left.Equals(right);
 
-    public override int GetHashCode() 
+    public override int GetHashCode()
         => HashCode.Combine(GetType(), Id);
 }
