4d357802582ec996c81fec90d2dcd153ba88c7b2
Amirhessam Pourhossein <amirhesamph81@gmail.com>
2024-05-12 14:33:06 +0330

    cleanup ICommentRepository.cs

--- src/Blogger.Domain/CommentAggregate/ICommentRepository.cs
+++ src/Blogger.Domain/CommentAggregate/ICommentRepository.cs
@@ -7,10 +7,7 @@ public interface ICommentRepository
     Task<Comment?> GetCommentByApproveLinkAsync(string link, CancellationToken cancellationToken);
     Task<IReadOnlyList<Comment>> GetApprovedArticleCommentsAsync(ArticleId articleId, CancellationToken cancellationToken);
     Task<IReadOnlyList<Reply>> GetApprovedRepliesAsync(CommentId commentId, CancellationToken cancellationToken);
-
-
     Task CreateAsync(Comment comment, CancellationToken cancellationToken);
-
     Task<Comment?> GetCommentByIdAsync(CommentId commentId, CancellationToken cancellationToken);
     Task SaveChangesAsync(CancellationToken cancellationToken);
 }
