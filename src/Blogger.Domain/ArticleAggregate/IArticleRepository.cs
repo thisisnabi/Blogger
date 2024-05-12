5938bf920eaa35ae8fc10f33bf17c28d01598468
Amirhessam Pourhossein <amirhesamph81@gmail.com>
2024-05-12 14:24:48 +0330

    cleanup IArticleRepository.cs

--- src/Blogger.Domain/ArticleAggregate/IArticleRepository.cs
+++ src/Blogger.Domain/ArticleAggregate/IArticleRepository.cs
@@ -2,7 +2,7 @@
 
 public interface IArticleRepository
 {
-    Task<IReadOnlyList<Tag>> GetPopularTagsAsync(int size,CancellationToken cancellationToken);
+    Task<IReadOnlyList<Tag>> GetPopularTagsAsync(int size, CancellationToken cancellationToken);
     Task<IReadOnlyList<Tag>> GetTagsAsync(CancellationToken cancellationToken);
     Task<bool> HasIdAsync(ArticleId articleId, CancellationToken cancellationToken);
     Task CreateAsync(Article article, CancellationToken cancellationToken);
@@ -10,7 +10,6 @@ public interface IArticleRepository
     void Delete(Article draft);
     Task<Article?> GetDraftByIdAsync(ArticleId draftId, CancellationToken cancellationToken);
     Task<Article?> GetArticleByIdAsync(ArticleId articleId, CancellationToken cancellationToken);
-     
     Task<IReadOnlyList<Article>> GetArchiveArticlesAsync(CancellationToken cancellationToken);
     Task<IReadOnlyList<Article>> GetLatestArticlesAsync(int pageNumber, int pageSize, CancellationToken cancellationToken);
     Task<IReadOnlyList<Article>> GetLatestArticlesAsync(Tag tag, CancellationToken cancellationToken);
