0bf4bea392b8452667b415dcefe14b3b5296a7f3
Amirhessam Pourhossein <amirhesamph81@gmail.com>
2024-05-12 14:24:16 +0330

    refactor DraftTagsMissingException, use primary constructor

--- src/Blogger.Domain/ArticleAggregate/DraftTagsMissingException.cs
+++ src/Blogger.Domain/ArticleAggregate/DraftTagsMissingException.cs
@@ -1,10 +1,6 @@
 ﻿using Blogger.Domain.Common.Exceptions;
 
 namespace Blogger.Domain.ArticleAggregate;
-public class DraftTagsMissingException : BlogException
+public class DraftTagsMissingException() : BlogException("Cannot publish draft without tags.")
 {
-    private const string _messages = "Cannot publish draft without tags.";
-    public DraftTagsMissingException() : base(_messages)
-    {
-    }
 }
