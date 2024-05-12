8611065583814a66288b4ac28bca9c66df308141
Amirhessam Pourhossein <amirhesamph81@gmail.com>
2024-05-12 14:23:19 +0330

    cleanup Author.cs

--- src/Blogger.Domain/ArticleAggregate/Author.cs
+++ src/Blogger.Domain/ArticleAggregate/Author.cs
@@ -1,11 +1,11 @@
 ﻿namespace Blogger.Domain.ArticleAggregate;
 public class Author : ValueObject<Author>
 {
-        public string FullName { get; init; }
+    public string FullName { get; init; }
 
-        public string Avatar { get; init; }
+    public string Avatar { get; init; }
 
-        public string JobTitle { get; init; }
+    public string JobTitle { get; init; }
 
     private Author(string fullName, string avatar, string jobTitle)
     {
@@ -28,7 +28,7 @@ public class Author : ValueObject<Author>
         ArgumentException.ThrowIfNullOrWhiteSpace(fullName);
         ArgumentException.ThrowIfNullOrWhiteSpace(avatar);
         ArgumentException.ThrowIfNullOrWhiteSpace(jobTitle);
-        
+
         return new Author(fullName, avatar, jobTitle);
     }
 }
